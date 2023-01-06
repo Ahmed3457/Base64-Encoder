0a1aa71dde52ef5dee6f8c789cada4453c734ac4
Ahmed <ahmedthecat3457@gmail.com>
2023-01-06 18:30:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Base64_Encoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EncodeBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.EncodedTextLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.EncodedTextLabel);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.EncodeBtn);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 365);
+            this.panel1.TabIndex = 0;
+            // 
+            // EncodeBtn
+            // 
+            this.EncodeBtn.BackColor = System.Drawing.Color.Silver;
+            this.EncodeBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.EncodeBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.EncodeBtn.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.EncodeBtn.Location = new System.Drawing.Point(0, 340);
+            this.EncodeBtn.Name = "EncodeBtn";
+            this.EncodeBtn.Size = new System.Drawing.Size(482, 23);
+            this.EncodeBtn.TabIndex = 1;
+            this.EncodeBtn.Text = "Encode/Decode";
+            this.EncodeBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.EncodeBtn.UseVisualStyleBackColor = false;
+            this.EncodeBtn.Click += new System.EventHandler(this.EncodeBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(0, 227);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 107);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(209, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Decode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // EncodedTextLabel
+            // 
+            this.EncodedTextLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.EncodedTextLabel.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.EncodedTextLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.EncodedTextLabel.Location = new System.Drawing.Point(0, 0);
+            this.EncodedTextLabel.Name = "EncodedTextLabel";
+            this.EncodedTextLabel.Size = new System.Drawing.Size(482, 184);
+            this.EncodedTextLabel.TabIndex = 0;
+            this.EncodedTextLabel.Text = "Click \"Encode/Decode\" To Encode/Decode Text From Base64";
+            this.EncodedTextLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 365);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Base64 Encoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button EncodeBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label EncodedTextLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base64_Encoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void btn2_click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EncodeBtn_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true){
+                string text = textBox1.Text;
+                byte[] encodedText = System.Convert.FromBase64String(text);
+                EncodedTextLabel.Text = System.Text.Encoding.UTF8.GetString(encodedText);
+            }
+            else
+            {
+                string text = textBox1.Text;
+                byte[] decodedText = System.Text.Encoding.UTF8.GetBytes(text);
+                EncodedTextLabel.Text = System.Convert.ToBase64String(decodedText);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
