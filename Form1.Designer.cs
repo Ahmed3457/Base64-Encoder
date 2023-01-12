e29b2554555eecfd8a461ef1f006ddf085a1b670
Ahmed <ahmedthecat3457@gmail.com>
2023-01-12 23:19:05 +0300

    Minor Improvments

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,120 +30,102 @@ namespace Base64_Encoder
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.EncodeBtn = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.EncodedTextLabel = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.EncodeBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // EncodedTextLabel
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.EncodedTextLabel);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.checkBox1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.EncodeBtn);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(484, 365);
-            this.panel1.TabIndex = 0;
+            this.EncodedTextLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EncodedTextLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.EncodedTextLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.EncodedTextLabel.Location = new System.Drawing.Point(3, 1);
+            this.EncodedTextLabel.Multiline = true;
+            this.EncodedTextLabel.Name = "EncodedTextLabel";
+            this.EncodedTextLabel.ReadOnly = true;
+            this.EncodedTextLabel.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.EncodedTextLabel.Size = new System.Drawing.Size(476, 154);
+            this.EncodedTextLabel.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(476, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Copy To Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(209, 196);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Decode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(3, 222);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(476, 107);
+            this.textBox1.TabIndex = 7;
             // 
             // EncodeBtn
             // 
             this.EncodeBtn.BackColor = System.Drawing.Color.Silver;
-            this.EncodeBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.EncodeBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.EncodeBtn.Font = new System.Drawing.Font("Segoe UI", 8F);
             this.EncodeBtn.Location = new System.Drawing.Point(0, 340);
             this.EncodeBtn.Name = "EncodeBtn";
             this.EncodeBtn.Size = new System.Drawing.Size(482, 23);
-            this.EncodeBtn.TabIndex = 1;
+            this.EncodeBtn.TabIndex = 6;
             this.EncodeBtn.Text = "Encode/Decode";
             this.EncodeBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.EncodeBtn.UseVisualStyleBackColor = false;
-            this.EncodeBtn.Click += new System.EventHandler(this.EncodeBtn_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(0, 227);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(479, 107);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(209, 195);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(62, 17);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "Decode";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(-1, 163);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(482, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Copy To Clipboard";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.copyToClipboard);
-            // 
-            // EncodedTextLabel
-            // 
-            this.EncodedTextLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.EncodedTextLabel.BackColor = System.Drawing.SystemColors.Control;
-            this.EncodedTextLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.EncodedTextLabel.Location = new System.Drawing.Point(3, 3);
-            this.EncodedTextLabel.Multiline = true;
-            this.EncodedTextLabel.Name = "EncodedTextLabel";
-            this.EncodedTextLabel.ReadOnly = true;
-            this.EncodedTextLabel.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.EncodedTextLabel.Size = new System.Drawing.Size(479, 154);
-            this.EncodedTextLabel.TabIndex = 5;
-            this.EncodedTextLabel.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 365);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.EncodedTextLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EncodeBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Base64 Encoder";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox EncodedTextLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button EncodeBtn;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox EncodedTextLabel;
     }
 }
 
